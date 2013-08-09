0c26d5a919fa8135311ba7bdaf2ebd452d9b643d
boovius <joshua.book@gmail.com>
2013-08-08 18:57:33 -0700

    add seed file for one full ritual sans creator, add site controller index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+new_ritual = Ritual.create({
+	title: 'Sleep Like a Saint',
+	target: 'Sleep',
+	objective: 'get better sleep',
+	description: 'The following ritual involves diligent setting of bed time with a relaxing cup of chamomile and reading.',
+	frequency: 'daily',
+	days_to_complete: '30',
+	steps: [{
+		name: 'set clock',
+		description: 'set bed time when home from work 8 hrs prior to wake up time',
+		location: 'home',
+		duration: '2',
+		supplies: [{
+			supply: '',
+			quantity: '',
+		}],
+	},
+	{
+		name: 'make tea',
+		description: 'make a cup of chamomile tea',
+		location: 'home',
+		duration: '5',
+		supplies: [{
+			supply: 'chamomile tea bag',
+			quantity: '1',
+		}],
+	},
+	{
+		name: 'read',
+		description: 'read until falling asleep',
+		location: 'bed',
+		duration: '15',
+		supplies: [{
+			supply: 'book',
+			quantity: '1',
+		}],
+	}],
+	min_rating_descrip: 'blood boiling anxiety without a wink',
+	max_rating_descrip: 'sleeping on clouds',
+	users_rating: '8.2'
+})
