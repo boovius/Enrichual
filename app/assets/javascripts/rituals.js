5ff69c4f86fc20b2029695b6f08e638b11ad3b44
boovius <joshua.book@gmail.com>
2013-08-12 13:57:08 -0700

    more fixes to still not working push state history rituals show view

--- app/assets/javascripts/rituals.js
+++ app/assets/javascripts/rituals.js
@@ -1,25 +1,22 @@
 $(function() {
   var showPartial = function(href) {
-    console.log('show partial called');
     $.ajax({
       method: 'GET',
       url: "/rituals/" + href,
       success: function(html) {
-        console.log('success!!');
         $('.step .description').empty();
-        $('#' + href.slice('/')).html(html);
+        console.log($('#' + href.slice('/')).html(html));
       }
     });
   };
   
   $(".step a").click(function(event) {
     var href = $(this).attr("href");
-    console.log(href);
+
+    console.log(href.slice('/step'));
     
     history.pushState({}, '', href);
-    var url = "/rituals/" + href;
 
-    console.log(url);
     showPartial(href);
     
     event.preventDefault();
