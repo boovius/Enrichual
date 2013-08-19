c31253c55c36eab4470883487e4a69e76f3fa561
boovius <joshua.book@gmail.com>
2013-08-19 12:25:33 -0700

    add debug helper function to app controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,8 +13,6 @@ class SearchController < ApplicationController
 
   	search_words = params[:query].split(' ')
 
-    debug('search words', search_words)
-
   	search_words.each do |word|
   		result = Program.where(target: word)
   		result.each do |r|
