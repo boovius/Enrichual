6f9919d521f32731948e175956a77c1aa750044d
boovius <joshua.book@gmail.com>
2013-08-21 17:02:41 -0700

    adjust app js to precompile assets, add precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery-2.0.3.min
-//= require jquery_ujs
+//= require jquery-ui-1.10.3.custom
 //= require turbolinks
 //= require foundation.min
 //= require_tree .
