d6b718146444ca8ef972a987db4d48d3b1138bd2
boovius <joshua.book@gmail.com>
2013-09-20 10:33:19 -0400

    fix update password issue

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -15,30 +15,7 @@ class PasswordController < ApplicationController
 	end
 
 	def update
-	  # Find the user with the unique reset code
-	  @user = User.first(code: params[:user][:code])
 
-	  # If the user exists and the code has not expired
-	  # then do the password update and set the code and
-	  # expiry to null (expire the reset page)
-	  if @user and @user.expires_at > Time.now
-	    @user.update_attributes(user_params)
-	    @user.code = nil
-	    @user.expires_at = nil
-
-	    if @user.save
-	      # Password update was successful, log the user in and
-	      # redirect to the home page
-	      session[:user_id] = @user.id
-	      redirect_to root_url, notice: "Password changed successfully."
-	    else
-	      # Password update failed, redirect to the login page
-	      redirect_to login_url, alert: "Reset link has expired. Please try again."
-	    end
-	  else
-	    # User not found or code expired, redirect to the login page
-	    redirect_to login_url, alert: "Reset link has expired. Please try again."
-	  end
 	end
 
 	private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,8 +27,29 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
+  def update
 
+    # If the user exists and the code has not expired
+    # then do the password update and set the code and
+    # expiry to null (expire the reset page)
+    if @user and @user.expires_at > Time.now
+      @user.update_attributes(user_params)
+      @user.code = nil
+      @user.expires_at = nil
+
+      if @user.save
+        # Password update was successful, log the user in and
+        # redirect to the home page
+        session[:user_id] = @user.id
+        redirect_to root_url, notice: "Password changed successfully."
+      else
+        # Password update failed, redirect to the login page
+        redirect_to login_url, alert: "Reset link has expired. Please try again."
+      end
+    else
+      # User not found or code expired, redirect to the login page
+      redirect_to login_url, alert: "Reset link has expired. Please try again."
+    end
   end
 
   def destroy
