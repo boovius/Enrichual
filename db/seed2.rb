f169fd5e989674713a89507c8fded750e3d24d5c
boovius <joshua.book@gmail.com>
2013-09-25 13:43:55 -0700

    add second seed file for more data

---
+++ db/seed2.rb
@@ -0,0 +1,43 @@
+relax = Program.create({
+	title: 'Walk Through Life Like a Monk',
+	target: 'relax',
+	objective: 'feel more relaxed everyday',
+	creator: '',
+	description: 'Go through your days with a deep sense of calm and peace.',
+	frequency: 'hourly',
+	days_to_complete: '30',
+	comments: [],
+	steps: [{
+		name: 'Ask Self',
+		description: "Ask yourself what you are feeling, what is going on in your body.  Write it down.  Track your mood. Identify stress points.",
+		location: 'anywhere',
+		duration: '3',
+		supplies: [{
+			supply: 'pen',
+			quantity: '1',
+		}],
+	},
+	{
+		name: 'Mantra Meditation',
+		description: "Repeat a simple mantra, such as the word 'calm' in meditation breathing in and out",
+		location: 'anywhere',
+		duration: '5',
+		supplies: [{
+			supply: '',
+			quantity: '',
+		}],
+	},
+	{
+		name: 'Set Intention',
+		description: 'Set your intention to be at total peace and calm until the next go round.',
+		location: 'anywhere',
+		duration: '1',
+		supplies: [{
+			supply: '',
+			quantity: '',
+		}],
+	}],
+	min_rating_descrip: "My heart is going to jump out of it's chest",
+	max_rating_descrip: "I'm floating on clouds all day",
+	users_rating: '7'
+})
